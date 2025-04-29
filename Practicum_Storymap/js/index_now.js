188c24c585b1db3354488f9d8ad2eb413090b58b
Akira Di Sandro <46532395+disandroa@users.noreply.github.com>
2025-04-29 02:19:17 -0400

    more images for now section
    initial map is on now section as well

--- Practicum_Storymap/js/index_now.js
+++ Practicum_Storymap/js/index_now.js
@@ -1,23 +1,26 @@
-// Initialize map of Philadelphia for slide 1
-const phillyMap = L.map('map-slide1').setView([39.9530, -75.1636], 12); // map centered at city hall
+// Initialize the map object
+const map = L.map('map', { scrollWheelZoom: false }).setView([39.95487450323784, -75.19999447856563], 13);
 
+// ## The Base Tile Layer
 const mapboxKey = 'pk.eyJ1IjoiY2hpYmlha2kiLCJhIjoiY20xODh2NTNqMTBvaDJqb2ptbjM4ZGViayJ9.un9M1_-S6kI8M0ktqZLz_Q';
-const mapboxStyle = 'mapbox/streets-v11';
+const mapboxStyle = 'mapbox/light-v11';
 
 L.tileLayer(`https://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/512/{z}/{x}/{y}{r}?access_token=${mapboxKey}`, {
   tileSize: 512,
   zoomOffset: -1,
+  detectRetina: true,
   maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(phillyMap);
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-// Initialize map of Chinatown and Callowhill for slide 2
-const studyareaMap = L.map('map-slide2').setView([39.95737510294851, -75.15808142492733], 12); // map centered at Chinatown Stitch, showing our whole study area
+// ## Interface Elements
+const slides = document.querySelectorAll('.slide');
+const container = document.querySelector('.slide-section');
 
-L.tileLayer(`https://api.mapbox.com/styles/v1/${mapboxStyle}/tiles/512/{z}/{x}/{y}{r}?access_token=${mapboxKey}`, {
-  tileSize: 512,
-  zoomOffset: -1,
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(studyareaMap);
+const deck = new SlideDeck(slides, map);
 
+// Scroll triggers slide change
+document.addEventListener('scroll', () => deck.calcCurrentSlideIndex());
+
+deck.preloadFeatureCollections();
+deck.syncMapToCurrentSlide();
